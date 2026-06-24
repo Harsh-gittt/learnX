7703afbaf0da95f41b51c26ff56623ae686680ab
Harsh-gittt <harshmain1226@gmail.com>
2026-06-25 02:46:40 +0530

    completed all the end points of /course

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -1,31 +1,137 @@
 const express = require("express");
 const { teacherAuthMiddleware } = require("../middlewares/teacherAuth");
+const { courseModel } = require("../db");
 
 const courseRouter = express.Router();
 
-courseRouter.post("/", teacherAuthMiddleware, function (req, res) {
-  res.json({message: "hi there"});
+courseRouter.post("/", teacherAuthMiddleware, async function (req, res) {
+  const teacherId = req.teacherId;
+  const {
+    title,
+    description,
+    price,
+    thumbnailUrl,
+    duration,
+    category,
+    createdAt,
+  } = req.body;
+
+  try {
+    const course = await courseModel.create({
+      title,
+      description,
+      price,
+      thumbnailUrl,
+      duration,
+      category,
+      createdAt,
+      creatorId: teacherId,
+    });
+
+    res.json({
+      message: "course added successfully!",
+      courseId: course._id,
+    });
+  } catch (e) {
+    res.json({
+      error: e.message,
+    });
+  }
 });
 
-courseRouter.get("/", function (req, res) {
-  res.json();
+courseRouter.get("/", async function (req, res) {
+  const courses = await courseModel.find({});
+
+  res.json({
+    message: "these are all available courses!",
+    courses: courses,
+  });
 });
 
-courseRouter.get("/:courseId", function (req, res) {
-  res.json();
+courseRouter.get("/:courseId", async function (req, res) {
+  const courseId = req.params.courseId;
+
+  const course = await courseModel.findOne({
+    _id: courseId,
+  });
+
+  res.json({
+    message: "details of course!",
+    course: course,
+  });
 });
 
-courseRouter.put("/:courseId", teacherAuthMiddleware, function (req, res) {
-  res.json();
-});
+courseRouter.put(
+  "/:courseId",
+  teacherAuthMiddleware,
+  async function (req, res) {
+    const teacherId = req.teacherId;
+    const courseId = req.params.courseId;
 
-courseRouter.delete("/:courseId", teacherAuthMiddleware, function (req, res) {
-  res.json();
-});
+    const updated = await courseModel.findOneAndUpdate(
+      {
+        creatorId: teacherId,
+        _id: courseId,
+      },
+      req.body,
+      {
+        new: true,
+      },
+    );
 
-courseRouter.get("/my-courses", teacherAuthMiddleware, function (req, res) {
-  res.json();
-});
+    if (updated) {
+      res.json({
+        message: "course updated successfully!",
+        course: updated,
+      });
+    } else {
+      res.json({
+        message: "course does not exist!",
+      });
+    }
+  },
+);
+
+courseRouter.delete(
+  "/:courseId",
+  teacherAuthMiddleware,
+  async function (req, res) {
+    const teacherId = req.teacherId;
+    const courseId = req.params.courseId;
+
+    const deleted = await courseModel.findOneAndDelete({
+      creatorId: teacherId,
+      _id: courseId,
+    });
+
+    if (deleted) {
+      res.json({
+        message: "course deleted successfully!",
+      });
+    } else {
+      res.json({
+        message: "course does not exist",
+      });
+    }
+  },
+);
+
+courseRouter.get(
+  "/my-courses",
+  teacherAuthMiddleware,
+  async function (req, res) {
+    const teacherId = req.teacherId;
+
+    const courses = await courseModel.find({
+      creatorId: teacherId,
+    });
+
+    res.json({
+      message: "these are your courses!",
+      courses: courses,
+    });
+  },
+);
 
 module.exports = {
   courseRouter: courseRouter,
